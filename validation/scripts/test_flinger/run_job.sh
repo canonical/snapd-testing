14e5bbec2970952dded6127fe0c92868279ae483
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-10-04 15:19:09 -0300

    testflinger support for jenkins and prodstack environment

--- validation/scripts/test_flinger/run_job.sh
+++ validation/scripts/test_flinger/run_job.sh
@@ -10,12 +10,32 @@ else
     sudo snap refresh testflinger-cli || true
 fi
 
-echo "Submitting job to testflinger"
-JOB_ID=$($TF_CLIENT submit -q $TF_JOB)
-echo "JOB_ID: ${JOB_ID}"
+# Moving the job to the snap data dir when using jenkins
+# This is needed due to user permissions needed to create
+# a directory when running the submit command
+# Also in other environments where sudo cannot be used we need
+# to run the snap from the home directory
+if [ "$(whoami)" = "jenkins" ]; then
+    sudo mv "$TF_JOB" "$TF_DATA"/job.yaml
+    TF_JOB="$TF_DATA"/job.yaml
 
-echo "Print job: $TF_JOB "
-cat $TF_JOB | tee $JOB_ID.job
+    echo "Submitting job to testflinger"
+    JOB_ID=$(sudo $TF_CLIENT submit -q $TF_JOB)
+    echo "JOB_ID: ${JOB_ID}"
 
-echo "Showing job data"
-$TF_CLIENT poll ${JOB_ID} | tee $JOB_ID.log
+    echo "Print job: $TF_JOB "
+    sudo cat $TF_JOB | tee $JOB_ID.job
+
+    echo "Showing job data"
+    sudo $TF_CLIENT poll ${JOB_ID} | tee $JOB_ID.log
+else
+    echo "Submitting job to testflinger"
+    JOB_ID=$($TF_CLIENT submit -q $TF_JOB)
+    echo "JOB_ID: ${JOB_ID}"
+
+    echo "Print job: $TF_JOB "
+    cat $TF_JOB | tee $JOB_ID.job
+
+    echo "Showing job data"
+    $TF_CLIENT poll ${JOB_ID} | tee $JOB_ID.log
+fi
