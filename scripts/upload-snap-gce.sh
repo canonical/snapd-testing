0ef539e1794fd46925a392dc75654f170cd24cfa
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-10-15 15:29:17 -0300

    new script to upload a snap to gce

---
+++ scripts/upload-snap-gce.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+snap=$1
+
+if [ -z "$snap" ]; then
+    echo "Snap cannot be empty, exiting..."
+    exit 1
+fi
+
+snap_name="$(basename $snap)"
+gsutil -o GSUtil:parallel_composite_upload_threshold=2000M cp "$snap" "gs://snapd-spread-tests/snaps/$snap_name"
