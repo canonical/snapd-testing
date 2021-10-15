25db4a05820c260a3149b114e978c9987180766f
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-10-15 16:29:06 -0300

    print the publid url for the snap uploaded to gce

--- scripts/upload-snap-gce.sh
+++ scripts/upload-snap-gce.sh
@@ -9,3 +9,4 @@ fi
 
 snap_name="$(basename $snap)"
 gsutil -o GSUtil:parallel_composite_upload_threshold=2000M cp "$snap" "gs://snapd-spread-tests/snaps/$snap_name"
+echo "public url: https://storage.googleapis.com/snapd-spread-tests/snaps/$snap_name"
