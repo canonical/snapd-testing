1a9cc05ef72a7ee0efe1de8358bf4d38b5320d7c
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-10-19 16:59:32 -0300

    allow to change the name when uploading to gce

--- scripts/upload-to-gce.sh
+++ scripts/upload-to-gce.sh
@@ -2,6 +2,7 @@
 
 file=$1
 subdir=${2:-'snaps'}
+file_name=${3:-''}
 
 if [ -z "$file" ]; then
     echo "File cannot be empty, exiting..."
@@ -13,6 +14,8 @@ if [ ! -f  "$file" ]; then
     exit 1
 fi
 
-file_name="$(basename $file)"
+if [ -z "$file_name" ]; then
+    file_name="$(basename $file)"
+fi
 gsutil -o GSUtil:parallel_composite_upload_threshold=2000M cp "$file" "gs://snapd-spread-tests/$subdir/$file_name"
 echo "public url: https://storage.googleapis.com/snapd-spread-tests/$subdir/$file_name"
