ea77ca8c7a028f1b324e1e846c903b5e1099551b
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-11-11 09:43:01 -0300

    Update the snpad board-ut for edge validation

--- validation/services/env/snapd-edge-smoke-amd64-snapd.sh
+++ validation/services/env/snapd-edge-smoke-amd64-snapd.sh
@@ -2,6 +2,6 @@
 
 export SNAP_UT=snapd
 export ARCH_UT=amd64
-export BOARD_UT=pc-amd64-refresh
+export BOARD_UT=pc-amd64-refresh-20
 export TARGET_CHANNEL=edge
 export TRELLO_CONFIG=trello_card_snapd_edge.yaml
