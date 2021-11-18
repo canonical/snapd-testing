0a3407ce70f11f2eb330e7a6aff51fae36da7499
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-11-18 17:12:08 -0300

    Updated scripts to support beta val with stable channel

--- validation/scripts/env/tf_snapd_amd64_refresh_20.sh
+++ validation/scripts/env/tf_snapd_amd64_refresh_20.sh
@@ -4,11 +4,11 @@
 
 export ARCH=${ARCH:-"amd64"}
 export PROJECT=${PROJECT:-"snapd"}
+export CHANNEL=${CHANNEL:-"stable"}
 export CORE_CHANNEL=${CORE_CHANNEL:-"stable"}
+export SNAPD_CHANNEL=${SNAPD_CHANNEL:-"beta"}
 export DEVICE_QUEUE=${DEVICE_QUEUE:-"maas-x86-node"}
 export DEVICE_DISTRO=${DEVICE_DISTRO:-"focal"}
-export CHANNEL=${CHANNEL:-"stable"}
-export SNAPD_CHANNEL=${SNAPD_CHANNEL:-"beta"}
 export SETUP=${SETUP:-""}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-20-64"}
 export SPREAD_PARAMS=${SPREAD_PARAMS:-"-v"}
