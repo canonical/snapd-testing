cc6e84f30626a34ebe582959a91931a932c51103
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-11-24 16:42:26 -0300

    Prepare the version when requesting the branch to git

--- scripts/git-branch.sh
+++ scripts/git-branch.sh
@@ -3,6 +3,10 @@
 VERSION=$1
 GITHUB_URL=${2:-https://github.com/snapcore/snapd.git}
 
+# Prepare the version format
+VERSION="${VERSION//16-/}"
+VERSION=$(echo "$VERSION" | cut -f1 -d "~")
+
 if git ls-remote --tags "$GITHUB_URL" "$VERSION" | grep -q "$VERSION"; then
     echo "$VERSION"
 elif git ls-remote --heads "$GITHUB_URL" "release/$VERSION" | grep -q "$VERSION"; then
