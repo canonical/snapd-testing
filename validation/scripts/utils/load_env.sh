decb5706be1a38d2ff747f655b08eb92e38c9bcc
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-11-29 17:48:00 -0300

    Fix order how variables are loaded

--- validation/scripts/utils/load_env.sh
+++ validation/scripts/utils/load_env.sh
@@ -6,13 +6,14 @@ echo "Loading environment variables"
 ENV_FILE="$1"
 if [ -f "$ENV_FILE" ]; then
     echo "Using environment file: $ENV_FILE"
-    . "$SCRIPTS_DIR/env/common.sh"
     . "$ENV_FILE"
 elif [ -f "$SCRIPTS_DIR/env/$ENV_FILE.sh" ]; then
     echo "Using environment file: $ENV_FILE"
-    . "$SCRIPTS_DIR/env/common.sh"
     . "$SCRIPTS_DIR/env/$ENV_FILE.sh"
 else
     echo "Environment file does not exist: $SCRIPTS_DIR/env/$ENV_FILE.sh"
     exit 1
-fi	
+fi
+
+echo "Loading common variables"
+. "$SCRIPTS_DIR/env/common.sh"
