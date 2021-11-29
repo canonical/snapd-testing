e83c48a943df72a5eb4054cefe7994fc1089c5dc
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-11-29 16:04:16 -0300

    missing change

--- validation/scripts/env/tf_cconf_db.sh
+++ validation/scripts/env/tf_cconf_db.sh
@@ -2,10 +2,13 @@
 
 export ARCH=${ARCH:-"armhf"}
 export PROJECT=${PROJECT:-"console-conf-tests"}
-export CHANNEL=${CHANNEL:-"beta"}
 export DEVICE_QUEUE=${DEVICE_QUEUE:-"dragonboard"}
+
+export CHANNEL=${CHANNEL:-"beta"}
+
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-64"}
 export SPREAD_ENV=${SPREAD_ENV:-"WIFI_READY=false"}
+
 export SKIP_REFRESH=${SKIP_REFRESH:-"true"}
 
 export TESTS_BACKEND=testflinger
