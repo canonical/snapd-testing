d1ddd4659ccd8300a6d428f2b8211f9628f6b9ab
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-11-30 17:38:06 -0300

    Fix tf jobs with new paths

--- validation/tests/test_flinger/create-job-device.sh
+++ validation/tests/test_flinger/create-job-device.sh
@@ -33,11 +33,11 @@ test_data:
         sudo apt install -y git curl sshpass jq unzip
         git clone "$JOBS_URL"
         (cd "$JOBS_PROJECT" && git checkout "$JOBS_BRANCH")
-        "$JOBS_PROJECT"/validation/scripts/utils/get-project.sh "$PROJECT_URL" "$PROJECT" "$BRANCH" "$COMMIT"
+        "$JOBS_PROJECT"/validation/tests/utils/get-project.sh "$PROJECT_URL" "$PROJECT" "$BRANCH" "$COMMIT"
         "$PROJECT"/tests/lib/external/prepare-ssh.sh "$DEVICE_IP" "$DEVICE_PORT" "$DEVICE_USER"
-        "$JOBS_PROJECT"/validation/scripts/utils/remote/add-root-key.sh "$DEVICE_IP" "$DEVICE_PORT" "$TEST_USER" "$TEST_PASS"
-        "$JOBS_PROJECT"/validation/scripts/utils/remote/refresh.sh "$DEVICE_IP" "$DEVICE_PORT" "$TEST_USER" "$TEST_PASS" "$CHANNEL" "$CORE_CHANNEL" "$SNAPD_CHANNEL"
-        "$JOBS_PROJECT"/validation/scripts/utils/register-device.sh "$DEVICE_IP" "$DEVICE_PORT" "$TEST_USER" "$TEST_PASS" "$REGISTER_EMAIL"
-        "$JOBS_PROJECT"/validation/scripts/utils/get-spread.sh "$SPREAD_URL"
-        "$JOBS_PROJECT"/validation/scripts/utils/run-spread.sh "$DEVICE_IP" "$DEVICE_PORT" "$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SKIP_TESTS" "$SPREAD_PARAMS"
+        "$JOBS_PROJECT"/validation/tests/utils/remote/add-root-key.sh "$DEVICE_IP" "$DEVICE_PORT" "$TEST_USER" "$TEST_PASS"
+        "$JOBS_PROJECT"/validation/tests/utils/remote/refresh.sh "$DEVICE_IP" "$DEVICE_PORT" "$TEST_USER" "$TEST_PASS" "$CHANNEL" "$CORE_CHANNEL" "$SNAPD_CHANNEL"
+        "$JOBS_PROJECT"/validation/tests/utils/register-device.sh "$DEVICE_IP" "$DEVICE_PORT" "$TEST_USER" "$TEST_PASS" "$REGISTER_EMAIL"
+        "$JOBS_PROJECT"/validation/tests/utils/get-spread.sh "$SPREAD_URL"
+        "$JOBS_PROJECT"/validation/tests/utils/run-spread.sh "$DEVICE_IP" "$DEVICE_PORT" "$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SKIP_TESTS" "$SPREAD_PARAMS"
 EOF
