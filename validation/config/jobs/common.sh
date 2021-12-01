a8bedda844646fd617f2391684d8c4cbaa39791a
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-12-01 08:51:59 -0300

    Update logs dir

--- validation/config/jobs/common.sh
+++ validation/config/jobs/common.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-WORKSPACE=${1:-$(pwd)}
-
-export SPREAD_LOG=$WORKSPACE/spread.log
-export SUMMARY_LOG=$WORKSPACE/summary.log
-export ERRORS_LOG=$WORKSPACE/errors.log
-export ATTACH_LOG=$WORKSPACE/attach.log
+export SPREAD_LOG=spread.log
+export SUMMARY_LOG=summary.log
+export ERRORS_LOG=errors.log
+export ATTACH_LOG=attach.log
 export SPREAD_URL=https://people.canonical.com/~sjcazzol/snappy/spreadSSHKeyFixed
