25e6b25754ef276dccae34df2ae110b4a0d7070d
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-02-01 00:03:52 -0300

    Using new spread in gce
    This has the fixedkey

--- validation/config/jobs/common.sh
+++ validation/config/jobs/common.sh
@@ -4,4 +4,4 @@ export SPREAD_LOG=spread.log
 export SUMMARY_LOG=summary.log
 export ERRORS_LOG=errors.log
 export ATTACH_LOG=attach.log
-export SPREAD_URL=https://people.canonical.com/~sjcazzol/snappy/spreadSSHKeyFixed
+export SPREAD_URL=https://storage.googleapis.com/snapd-spread-tests/spread/spread-fixedkey-amd64.tar.gz
