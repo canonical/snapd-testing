ac3e0505f73576350797f956b7b36f37d1a9366c
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-02-01 16:12:35 -0300

    Test remodel without device registration

--- validation/tests/test_flinger/create-job-vm.sh
+++ validation/tests/test_flinger/create-job-vm.sh
@@ -28,7 +28,7 @@ test_data:
         ssh ${DEVICE_USER}@${DEVICE_IP} "sudo $JOBS_PROJECT/validation/tests/utils/remote/create-vm.sh \"$ARCH\" \"$IMAGE_URL\" \"$USER_ASSERTION_URL\" \"$BUILD_SNAPD\""
         ssh ${DEVICE_USER}@${DEVICE_IP} "$JOBS_PROJECT/validation/tests/utils/remote/add-root-key.sh \"$HOST\" \"$PORT\" \"$TEST_USER\" \"$TEST_PASS\""
         ssh ${DEVICE_USER}@${DEVICE_IP} "$JOBS_PROJECT/validation/tests/utils/remote/refresh.sh \"$HOST\" \"$PORT\" \"$TEST_USER\" \"$TEST_PASS\" \"$CHANNEL\" \"$CORE_CHANNEL\" \"$SNAPD_CHANNEL\""
-        ssh ${DEVICE_USER}@${DEVICE_IP} "$JOBS_PROJECT/validation/tests/utils/register-device.sh \"$HOST\" \"$PORT\" \"$TEST_USER\" \"$TEST_PASS\" \"$REGISTER_EMAIL\""
+        #ssh ${DEVICE_USER}@${DEVICE_IP} "$JOBS_PROJECT/validation/tests/utils/register-device.sh \"$HOST\" \"$PORT\" \"$TEST_USER\" \"$TEST_PASS\" \"$REGISTER_EMAIL\""
         ssh ${DEVICE_USER}@${DEVICE_IP} "$JOBS_PROJECT/validation/tests/utils/get-spread.sh \"$SPREAD_URL\""
         ssh ${DEVICE_USER}@${DEVICE_IP} "$JOBS_PROJECT/validation/tests/utils/run-spread.sh \"$HOST\" \"$PORT\" \"$PROJECT\" \"$SPREAD_TESTS\" \"$SPREAD_ENV\" \"$SKIP_TESTS\" \"$SPREAD_PARAMS\""
 EOF
