a4ff82d4588f78bd7e422afe13bf9b7670a8b326
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-03-04 09:16:44 +0100

    Removing remodel tests again from validation

--- validation/config/tests/tf_snapd_amd64_18.sh
+++ validation/config/tests/tf_snapd_amd64_18.sh
@@ -15,7 +15,7 @@ export SNAPD_CHANNEL=${SNAPD_CHANNEL:-"beta"}
 export BRANCH=${BRANCH:-"beta"}
 
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-18-64"}
-export SKIP_TESTS=${SKIP_TESTS:-"tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided"}
+export SKIP_TESTS=${SKIP_TESTS:-"tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided,tests/core/remodel,tests/core/remodel-base,tests/core/remodel-kernel,tests/core/remodel-gadget"}
 
 export TESTS_BACKEND=testflinger
 export TESTS_DEVICE=vm
