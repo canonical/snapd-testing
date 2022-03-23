4aa1052709cf20ba3aaa8f269a70403a9b791707
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-03-23 17:42:28 -0300

    skip failover for uc16 as well

--- validation/config/tests/tf_snapd_amd64.sh
+++ validation/config/tests/tf_snapd_amd64.sh
@@ -15,7 +15,7 @@ export SNAPD_CHANNEL=${SNAPD_CHANNEL:-"beta"}
 export BRANCH=${BRANCH:-"beta"}
 
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-64"}
-export SKIP_TESTS=${SKIP_TESTS:-"tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided"}
+export SKIP_TESTS=${SKIP_TESTS:-"tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided,tests/core/failover"}
 
 export TESTS_BACKEND=testflinger
 export TESTS_DEVICE=vm
