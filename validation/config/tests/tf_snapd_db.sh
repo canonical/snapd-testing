82439436e455caedfc8df96b1bfa815526b98f24
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-04-11 11:38:49 -0300

    skip refresh or db

--- validation/config/tests/tf_snapd_db.sh
+++ validation/config/tests/tf_snapd_db.sh
@@ -16,5 +16,6 @@ export BRANCH=${BRANCH:-"beta"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-16-arm-64:tests/"}
 export SKIP_TESTS=${SKIP_TESTS:-"tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided,tests/core/failover"}
 
+export SKIP_REFRESH=${SKIP_REFRESH:-"true"}
 export TESTS_BACKEND=testflinger
 export TESTS_DEVICE=device
--- validation/config/tests/tf_snapd_db_18.sh
+++ validation/config/tests/tf_snapd_db_18.sh
@@ -16,5 +16,6 @@ export BRANCH=${BRANCH:-"beta"}
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-18-arm-64:tests/"}
 export SKIP_TESTS=${SKIP_TESTS:-"tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided,tests/core/remodel,tests/core/remodel-base,tests/core/remodel-kernel,tests/core/remodel-gadget,tests/core/failover,tests/main/interfaces-network-manager"}
 
+export SKIP_REFRESH=${SKIP_REFRESH:-"true"}
 export TESTS_BACKEND=testflinger
 export TESTS_DEVICE=device
