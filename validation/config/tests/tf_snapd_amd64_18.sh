4b1f986c56f2d6a120c672660891ebae3ea4dce1
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-04-11 11:29:36 -0300

    disable interfaces-network-manager on uc18

--- validation/config/tests/tf_snapd_amd64_18.sh
+++ validation/config/tests/tf_snapd_amd64_18.sh
@@ -15,7 +15,7 @@ export SNAPD_CHANNEL=${SNAPD_CHANNEL:-"beta"}
 export BRANCH=${BRANCH:-"beta"}
 
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-18-64"}
-export SKIP_TESTS=${SKIP_TESTS:-"tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided,tests/core/remodel,tests/core/remodel-base,tests/core/remodel-kernel,tests/core/remodel-gadget,tests/core/failover"}
+export SKIP_TESTS=${SKIP_TESTS:-"tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided,tests/core/remodel,tests/core/remodel-base,tests/core/remodel-kernel,tests/core/remodel-gadget,tests/core/failover,tests/main/interfaces-network-manager"}
 
 export TESTS_BACKEND=testflinger
 export TESTS_DEVICE=vm
