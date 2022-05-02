9228e2bb63109cc9068913fb8c4f2ffd8f579a66
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-05-02 14:32:00 -0300

    Updated the configuration to run beta validation for uc22

--- validation/config/tests/tf_snapd_amd64_22.sh
+++ validation/config/tests/tf_snapd_amd64_22.sh
@@ -6,10 +6,10 @@ export PROJECT=${PROJECT:-"snapd"}
 export PROJECT_URL=${PROJECT_URL:-"https://github.com/snapcore/snapd.git"}
 
 export DEVICE_QUEUE=${DEVICE_QUEUE:-"maas-x86-node"}
-export DEVICE_DISTRO=${DEVICE_DISTRO:-"focal"}
+export DEVICE_DISTRO=${DEVICE_DISTRO:-"jammy"}
 
-export CHANNEL=${CHANNEL:-"stable"}
-export CORE_CHANNEL=${CORE_CHANNEL:-"stable"}
+export CHANNEL=${CHANNEL:-"edge"}
+export CORE_CHANNEL=${CORE_CHANNEL:-"edge"}
 export SNAPD_CHANNEL=${SNAPD_CHANNEL:-"beta"}
 
 export BRANCH=${BRANCH:-"beta"}
