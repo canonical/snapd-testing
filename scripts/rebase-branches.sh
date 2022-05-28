71e9ff86e5b2ec0cabbb6526b8b43945c749b0b5
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-05-28 10:51:57 -0300

    Add prefix to rebase branches script

--- scripts/rebase-branches.sh
+++ scripts/rebase-branches.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+BRANCH_PREFIX=${1:-""}
+
 git for-each-ref 'refs/heads/*' | \
 while read rev type ref; do
     branch=$(expr "$ref" : 'refs/heads/\(.*\)' )
     revs=$(git rev-list $rev..master)
     echo "checking branch: $branch"
+    if [[ $branch != "$BRANCH_PREFIX"* ]]; then
+        echo "branch: $branch doesn't match the specified prefix $BRANCH_PREFIX"
+        continue
+    fi
     if [ -n "$revs" ]; then
         echo $branch needs update
-        git diff --summary --shortstat -M -C -C $rev master
         git checkout $branch
         git rebase master
         git push -f
