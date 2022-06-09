29e6f10f73a1cad8eab76615b519c3092140c530
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-06-09 16:19:05 -0300

    Update again the vars used for create teh vm

--- validation/tests/utils/remote/create-vm.sh
+++ validation/tests/utils/remote/create-vm.sh
@@ -191,8 +191,8 @@ if [ "$CURR_SYSTEM" = focal ] || [ "$CURR_SYSTEM" = jammy ]; then
     if [ "$CURR_SYSTEM" = jammy ]; then
         wget https://storage.googleapis.com/snapd-spread-tests/dependencies/OVMF_CODE.secboot.fd
         mv OVMF_CODE.secboot.fd /usr/share/OVMF/OVMF_CODE.secboot.fd
-        wget https://storage.googleapis.com/snapd-spread-tests/dependencies/OVMF_VARS.snakeoil.fd
-        mv OVMF_VARS.snakeoil.fd /usr/share/OVMF/OVMF_VARS.snakeoil.fd
+        wget https://storage.googleapis.com/snapd-spread-tests/dependencies/OVMF_VARS.ms.fd
+        mv OVMF_VARS.ms.fd /usr/share/OVMF/OVMF_VARS.ms.fd
     fi
 
     cp /usr/share/OVMF/OVMF_VARS.snakeoil.fd "$WORK_DIR"/OVMF_VARS.snakeoil.fd
@@ -200,7 +200,7 @@ if [ "$CURR_SYSTEM" = focal ] || [ "$CURR_SYSTEM" = jammy ]; then
         -machine ubuntu-q35,accel=kvm -global ICH9-LPC.disable_s3=1 \
         -netdev user,id=mynet0,hostfwd=tcp::$PORT-:22 -device virtio-net-pci,netdev=mynet0 \
         -drive file=/usr/share/OVMF/OVMF_CODE.secboot.fd,if=pflash,format=raw,unit=0,readonly=on \
-        -drive file=$WORK_DIR/OVMF_VARS.snakeoil.fd,if=pflash,format=raw,unit=1 \
+        -drive file=$WORK_DIR/OVMF_VARS.ms.fd,if=pflash,format=raw,unit=1 \
         -chardev socket,id=chrtpm,path=/var/snap/test-snapd-swtpm/current/swtpm-sock \
         -tpmdev emulator,id=tpm0,chardev=chrtpm -device tpm-tis,tpmdev=tpm0 \
         -drive file=$WORK_DIR/ubuntu-core.img,cache=none,format=raw,id=disk1,if=none \
