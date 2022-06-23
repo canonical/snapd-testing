ae72da1bde627ab814e8d50ca27840a291d73421
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-06-23 08:39:04 -0300

    Files needed to support core22 edge validation

---
+++ validation/config/jobs/snapd-edge-smoke-amd64-core22.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export SNAP_UT=core22
+export ARCH_UT=amd64
+export BOARD_UT=pc-amd64
+export TARGET_CHANNEL=edge
+export JIRA_CONFIG=jira_card_core22.yaml
---
+++ validation/config/jobs/snapd-edge-smoke-pi3-core22.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export SNAP_UT=core22
+export ARCH_UT=arm32
+export BOARD_UT=pi3
+export TARGET_CHANNEL=edge
+export JIRA_CONFIG=jira_card_core22.yaml
