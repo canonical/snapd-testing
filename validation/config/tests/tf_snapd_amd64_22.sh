302c18b5011efaa90b87c70dd0b371751f727829
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2022-09-16 18:10:12 -0300

    skip the test tests/core/persistent-journal-namespace in uc20 and uc22

--- validation/config/tests/tf_snapd_amd64_22.sh
+++ validation/config/tests/tf_snapd_amd64_22.sh
@@ -16,7 +16,7 @@ export BRANCH=${BRANCH:-"beta"}
 export VERSION=${VERSION:-"uc22"}
 
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-22-64"}
-export SKIP_TESTS=${SKIP_TESTS:-"tests/core/uc20-recovery,tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided"}
+export SKIP_TESTS=${SKIP_TESTS:-"tests/core/uc20-recovery,tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided,tests/core/persistent-journal-namespace"}
 
 export TESTS_BACKEND=testflinger
 export TESTS_DEVICE=vm
--- validation/config/tests/tf_snapd_pi3_20.sh
+++ validation/config/tests/tf_snapd_pi3_20.sh
@@ -15,7 +15,7 @@ export BRANCH=${BRANCH:-"beta"}
 export VERSION=${VERSION:-"uc20"}
 
 export SPREAD_TESTS=${SPREAD_TESTS:-"external:ubuntu-core-20-arm-32:tests/"}
-export SKIP_TESTS=${SKIP_TESTS:-"tests/core/uc20-recovery,tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided"}
+export SKIP_TESTS=${SKIP_TESTS:-"tests/core/uc20-recovery,tests/main/interfaces-many-snap-provided,tests/main/interfaces-many-core-provided,tests/core/persistent-journal-namespace"}
 
 export TESTS_BACKEND=testflinger
 export TESTS_DEVICE=device
