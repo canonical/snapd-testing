4ea17f6f3bb7104f6b8daf988cd04809bbc9e84c
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2023-06-16 15:31:24 -0300

    Force using dawson-003 for nuc
    It is to make sure the nuc works with snapd

--- validation/config/tests/tf_snapd_nuc_20.sh
+++ validation/config/tests/tf_snapd_nuc_20.sh
@@ -5,7 +5,8 @@ export ARCH=${ARCH:-"amd64"}
 export PROJECT=${PROJECT:-"snapd"}
 export PROJECT_URL=${PROJECT_URL:-"https://github.com/snapcore/snapd.git"}
 
-export DEVICE_QUEUE=${DEVICE_QUEUE:-"dawson-i-uc20-fde"}
+#export DEVICE_QUEUE=${DEVICE_QUEUE:-"dawson-i-uc20-fde"}
+export DEVICE_QUEUE=${DEVICE_QUEUE:-"dawson-003"}
 
 export CHANNEL=${CHANNEL:-"beta"}
 export CORE_CHANNEL=${CORE_CHANNEL:-"stable"}
