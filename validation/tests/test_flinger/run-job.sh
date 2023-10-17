c94061e61056c44899b36139334dd66b8e4215de
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2023-10-17 09:03:08 -0300

    no listing queues anymore
    Traceback (most recent call last):
    File "/snap/testflinger-cli/158/bin/testflinger-cli", line 8, in
    <module>
        sys.exit(cli())
    File
    "/snap/testflinger-cli/158/lib/python3.8/site-packages/testflinger_cli/__init__.py",
    line 47, in cli
        tfcli.run()
    File
    "/snap/testflinger-cli/158/lib/python3.8/site-packages/testflinger_cli/__init__.py",
    line 142, in run
        raise SystemExit(self.args.func())
    File
    "/snap/testflinger-cli/158/lib/python3.8/site-packages/testflinger_cli/__init__.py",
    line 521, in list_queues
        for name, description in sorted(queues.items()):
    UnboundLocalError: local variable 'queues' referenced before assignment

--- validation/tests/test_flinger/run-job.sh
+++ validation/tests/test_flinger/run-job.sh
@@ -11,9 +11,6 @@ echo "NO_PROXY=$NO_PROXY"
 echo "HTTP_PROXY=$HTTP_PROXY"
 echo "HTTPS_PROXY=$HTTPS_PROXY"
 
-echo "listing queues"
-HTTP_PROXY=$HTTP_PROXY "$TF_CLIENT" list-queues
-
 echo "Submitting job to testflinger"
 JOB_ID=$($TF_CLIENT submit -q $TF_JOB)
 echo "JOB_ID: ${JOB_ID}"
