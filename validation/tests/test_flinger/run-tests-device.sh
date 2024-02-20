6a4cf9ffca05dc0bc462918429bf024db915a6f6
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2024-02-20 09:53:24 -0300

    more changes

--- validation/tests/test_flinger/run-tests-device.sh
+++ validation/tests/test_flinger/run-tests-device.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-CHANNEL=$1
-
-
 "$JOBS_PROJECT"/validation/tests/utils/get-project.sh "$PROJECT_URL" "$PROJECT" "$BRANCH" "$VERSION" "$ARCH" "$COMMIT"
-sed "/^backends:/r validation/config/spread/tf_${CHANNEL}_spread.yaml" -i "$PROJECT"/spread.yaml
+sed "/^backends:/r "$JOBS_PROJECT"/validation/config/spread/tf_${CHANNEL}_spread.yaml" -i "$PROJECT"/spread.yaml
 "$JOBS_PROJECT"/validation/tests/utils/run-spread.sh "$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SKIP_TESTS" "$SPREAD_PARAMS"
 
