32a5e7b5cfc0743f80630e86fd17a27c698119a8
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2024-02-29 15:27:48 -0300

    print test results in google validation

--- validation/tests/run-tests.sh
+++ validation/tests/run-tests.sh
@@ -24,6 +24,7 @@ if [ "$ARCH_UT" = "amd64" ]; then
     "$VALIDATION_DIR"/tests/utils/run-spread.sh "$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SKIP_TESTS" "$SPREAD_PARAMS"
     LOG=$(find "$PROJECT"/artifacts -name spread.log)
     cp "$LOG" "$SPREAD_LOG"
+    cat "$SPREAD_LOG"
 else
     "$VALIDATION_DIR"/tests/utils/get-project.sh "$PROJECT_URL" "$PROJECT" "$BRANCH" "$VERSION_UT" "$ARCH_UT"
     sed "/^backends:/r "$VALIDATION_DIR"/config/spread/tf_${CHANNEL}_spread.yaml" -i "$PROJECT"/spread.yaml
