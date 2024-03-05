c0b20643871a25b01040288b3b8db3d0d82eb43f
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2024-03-05 09:38:29 -0300

    update tests used for edge validation

--- validation/config/jobs/snapd-edge-smoke-pi4-core20.sh
+++ validation/config/jobs/snapd-edge-smoke-pi4-core20.sh
@@ -8,7 +8,7 @@ export PROJECT=snapd
 export BRANCH=master
 export CHANNEL=edge
 
-export SPREAD_TESTS="testflinger:ubuntu-core-20-rpi4:tests/"
+export SPREAD_TESTS="testflinger:ubuntu-core-20-rpi4:tests/smoke/"
 export SPREAD_ENV=
 export SPREAD_PARAMS=
 export SPREAD_SKIP=
