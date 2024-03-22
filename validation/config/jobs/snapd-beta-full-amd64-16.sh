613c347ccceb7b684638eb8ebc3fdb64eb3fce8b
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2024-03-22 14:46:40 -0300

    allow reexecution for nested tests in amd64

--- validation/config/jobs/snapd-beta-full-amd64-16.sh
+++ validation/config/jobs/snapd-beta-full-amd64-16.sh
@@ -8,7 +8,8 @@ export PROJECT=snapd
 export BRANCH=beta
 export CHANNEL=beta
 
+export NESTED_SPREAD_TESTS="${SPREAD_TESTS:-"external:ubuntu-core-16-64:"}"
 export SPREAD_TESTS="google-nested-dev:ubuntu-16.04-64:tests/nested/manual/run-spread:custom"
-export SPREAD_ENV="NESTED_CUSTOM_IMAGE_URL=https://storage.googleapis.com/snapd-spread-tests/images/pc-amd64-16-stable-core_beta/pc.img.xz NESTED_SPREAD_TESTS=external:ubuntu-core-16-64:tests/"
+export SPREAD_ENV="NESTED_CUSTOM_IMAGE_URL=https://storage.googleapis.com/snapd-spread-tests/images/pc-amd64-16-stable-core_beta/pc.img.xz NESTED_SPREAD_TESTS=\"$NESTED_SPREAD_TESTS\""
 export SPREAD_PARAMS="-artifacts=./artifacts"
 export SPREAD_SKIP=
--- validation/config/jobs/snapd-beta-full-amd64-22.sh
+++ validation/config/jobs/snapd-beta-full-amd64-22.sh
@@ -8,7 +8,8 @@ export PROJECT=snapd
 export BRANCH=beta
 export CHANNEL=beta
 
+export NESTED_SPREAD_TESTS="${SPREAD_TESTS:-"external:ubuntu-core-22-64:"}"
 export SPREAD_TESTS="google-nested-dev:ubuntu-22.04-64:tests/nested/manual/run-spread:custom"
-export SPREAD_ENV="NESTED_CUSTOM_IMAGE_URL=https://storage.googleapis.com/snapd-spread-tests/images/pc-amd64-22-stable-snapd_beta/pc.img.xz NESTED_SPREAD_TESTS=external:ubuntu-core-22-64:tests/"
+export SPREAD_ENV="NESTED_CUSTOM_IMAGE_URL=https://storage.googleapis.com/snapd-spread-tests/images/pc-amd64-22-stable-snapd_beta/pc.img.xz  NESTED_SPREAD_TESTS=\"$NESTED_SPREAD_TESTS\""
 export SPREAD_PARAMS="-artifacts=./artifacts"
 export SPREAD_SKIP=
