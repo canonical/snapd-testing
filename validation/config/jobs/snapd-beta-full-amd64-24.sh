351e4c61bf33c0f7969a22f1e3a8bf2aef9fbf93
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2024-04-24 11:12:33 -0300

    Update urls to validate uc24 beta images

--- validation/config/jobs/snapd-beta-full-amd64-24.sh
+++ validation/config/jobs/snapd-beta-full-amd64-24.sh
@@ -10,6 +10,6 @@ export CHANNEL=beta
 
 export NESTED_SPREAD_TESTS="${SPREAD_TESTS:-"external:ubuntu-core-24-64:"}"
 export SPREAD_TESTS="google-nested-dev:ubuntu-24.04-64:tests/nested/manual/run-spread:custom"
-export SPREAD_ENV="NESTED_CUSTOM_IMAGE_URL=https://storage.googleapis.com/snapd-spread-tests/images/pc-amd64-24-beta-snapd_beta/pc.img.xz  NESTED_SPREAD_TESTS=$NESTED_SPREAD_TESTS"
+export SPREAD_ENV="NESTED_CUSTOM_IMAGE_URL=https://cdimage.ubuntu.com/ubuntu-core/24/beta/pending/ubuntu-core-24-amd64.img.xz  NESTED_SPREAD_TESTS=$NESTED_SPREAD_TESTS"
 export SPREAD_PARAMS="-artifacts=./artifacts"
 export SPREAD_SKIP=
