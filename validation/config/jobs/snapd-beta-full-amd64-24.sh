23511a515b2c76a4e4cf053bfb54a93cb2360185
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2024-04-24 17:27:58 -0300

    use dangerous image for uc24

--- validation/config/jobs/snapd-beta-full-amd64-24.sh
+++ validation/config/jobs/snapd-beta-full-amd64-24.sh
@@ -13,6 +13,6 @@ export PROJECT_URL=https://github.com/sergiocazzolato/snapd.git
 
 export NESTED_SPREAD_TESTS="${SPREAD_TESTS:-"external:ubuntu-core-24-64:"}"
 export SPREAD_TESTS="google-nested-dev:ubuntu-24.04-64:tests/nested/manual/run-spread:custom"
-export SPREAD_ENV="NESTED_CUSTOM_IMAGE_URL=https://cdimage.ubuntu.com/ubuntu-core/24/beta/pending/ubuntu-core-24-amd64.img.xz  NESTED_SPREAD_TESTS=$NESTED_SPREAD_TESTS"
+export SPREAD_ENV="NESTED_CUSTOM_IMAGE_URL=https://cdimage.ubuntu.com/ubuntu-core/24/dangerous-beta/pending/ubuntu-core-24-amd64.img.xz NESTED_SPREAD_TESTS=$NESTED_SPREAD_TESTS"
 export SPREAD_PARAMS="-artifacts=./artifacts"
 export SPREAD_SKIP=
