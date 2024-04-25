e08ba04e959f558279b05ebeb4cf7043333959a0
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2024-04-25 08:56:08 -0300

    fix var which determines which tests to skip

--- validation/tests/run-tests.sh
+++ validation/tests/run-tests.sh
@@ -21,11 +21,11 @@ fi
 
 "$VALIDATION_DIR"/tests/utils/get-project.sh "$PROJECT_URL" "$PROJECT" "$VALIDATION_DIR" "$BRANCH" "$VERSION_UT" "$ARCH_UT"
 if [ "$ARCH_UT" = "amd64" ]; then
-    "$VALIDATION_DIR"/tests/utils/run-spread.sh "$VALIDATION_DIR/$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SKIP_TESTS" "$SPREAD_PARAMS"
+    "$VALIDATION_DIR"/tests/utils/run-spread.sh "$VALIDATION_DIR/$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SPREAD_SKIP" "$SPREAD_PARAMS"
     LOG=$(find "$VALIDATION_DIR/$PROJECT"/artifacts -name spread.log)
     cp "$LOG" "$SPREAD_LOG"
     cat "$SPREAD_LOG"
 else
     sed "/^backends:/r "$VALIDATION_DIR"/config/spread/tf_${CHANNEL}_spread.yaml" -i "$VALIDATION_DIR/$PROJECT"/spread.yaml
-    "$VALIDATION_DIR"/tests/utils/run-spread.sh "$VALIDATION_DIR/$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SKIP_TESTS" "$SPREAD_PARAMS" | tee "$SPREAD_LOG"
+    "$VALIDATION_DIR"/tests/utils/run-spread.sh "$VALIDATION_DIR/$PROJECT" "$SPREAD_TESTS" "$SPREAD_ENV" "$SPREAD_SKIP" "$SPREAD_PARAMS" | tee "$SPREAD_LOG"
 fi
