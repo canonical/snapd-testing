e22d474d26936f4314ea8743699b7b347a212341
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2024-04-29 17:30:26 -0300

    update the name of the arm systems for edge and beta validation

--- validation/config/jobs/snapd-edge-smoke-pi4-uc20.sh
+++ validation/config/jobs/snapd-edge-smoke-pi4-uc20.sh
@@ -9,7 +9,7 @@ export PROJECT=snapd
 export BRANCH=master
 export CHANNEL=edge
 
-export SPREAD_TESTS="testflinger:ubuntu-core-20-rpi4:tests/smoke/"
+export SPREAD_TESTS="testflinger:ubuntu-core-20-arm64-rpi4:tests/smoke/"
 export SPREAD_ENV="IMAGE_URL=http://cdimage.ubuntu.com/ubuntu-core/20/dangerous-edge/pending/ubuntu-core-20-arm64+raspi.img.xz"
 export SPREAD_PARAMS=
 export SPREAD_SKIP=
