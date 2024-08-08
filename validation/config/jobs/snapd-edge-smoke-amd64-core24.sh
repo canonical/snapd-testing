468de8aba19feca88f795906f92235fd4374ff3a
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2024-08-08 10:14:31 -0300

    udpated images urls for uc24

--- validation/config/jobs/snapd-edge-smoke-amd64-core24.sh
+++ validation/config/jobs/snapd-edge-smoke-amd64-core24.sh
@@ -9,6 +9,6 @@ export BRANCH=master
 export CHANNEL=edge
 
 export SPREAD_TESTS="google-nested-dev:ubuntu-24.04-64:tests/nested/manual/run-spread:custom"
-export SPREAD_ENV="NESTED_CUSTOM_IMAGE_URL=https://storage.googleapis.com/snapd-spread-tests/images/pc-amd64-24-edge-core24_edge/pc.img.xz NESTED_SPREAD_TESTS=external:ubuntu-core-24-64:tests/smoke/"
+export SPREAD_ENV="NESTED_CUSTOM_IMAGE_URL=https://storage.googleapis.com/snapd-spread-tests/images/pc-amd64-24-stable-core24_edge/pc.img.xz NESTED_SPREAD_TESTS=external:ubuntu-core-24-64:tests/smoke/"
 export SPREAD_PARAMS="-artifacts=./artifacts"
 export SPREAD_SKIP=
