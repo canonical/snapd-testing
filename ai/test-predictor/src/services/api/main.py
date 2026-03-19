c568be3cd60557ebed73917e45d31e716880916d
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2026-03-19 15:59:25 -0300

    remove batch training from main

--- ai/test-predictor/src/services/api/main.py
+++ ai/test-predictor/src/services/api/main.py
@@ -1,12 +1,11 @@
 import logging
 import os
 
-from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 
 from src.services.api.ingestion import ingestion_bp
 from src.services.api.explorer import explorer_bp
-from src.services.api.trainer import trainer_bp, perform_training_cycle
+from src.services.api.trainer import trainer_bp
 
 from common import config
 from common.config import setup_logging
@@ -28,18 +27,3 @@ app.model_manager = manager
 app.register_blueprint(ingestion_bp)
 app.register_blueprint(trainer_bp)
 app.register_blueprint(explorer_bp)
-
-
-def scheduled_training_job():
-    """Background task to run every TRAIN_INTERVAL_MINUTES"""
-    # We use a manual app context if we need to access current_app
-    with app.app_context():
-        logger.info("Scheduled Job: Checking for new data...")
-        # Reuses the same logic as the /train endpoint
-        perform_training_cycle(app)
-
-# Initialize Scheduler
-scheduler = BackgroundScheduler(daemon=True)
-# Runs every TRAIN_INTERVAL_MINUTES
-scheduler.add_job(scheduled_training_job, 'interval', minutes=config.TRAIN_INTERVAL_MINUTES)
-scheduler.start()
