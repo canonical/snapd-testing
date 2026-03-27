f1f904bd202327d1314a1ce6953d0a349e9a6ac7
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2026-03-27 13:13:31 -0300

    balance weights

--- ai/test-predictor/src/common/config.py
+++ ai/test-predictor/src/common/config.py
@@ -64,7 +64,7 @@ TRAINING_MAX_FILES = 200
 TRAINING_CHUNKS_SIZE = 30000
 # Weights used during training to handle class imbalance. The model will "pay more attention" to the underrepresented class.
 WEIGHT_POSITIVE_CLASS = 1.0
-WEIGHT_NEGATIVE_CLASS = 2.0
+WEIGHT_NEGATIVE_CLASS = 1.0
 
 # Prediction Settings
 # Verbose (0): No output. 1: Progress bar. 2: One line per epoch.
