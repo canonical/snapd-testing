7d2272592165769deaa1b6b58daa6c0afb0104d4
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2026-03-27 13:43:43 -0300

    reduce the size of the sequence

--- ai/test-predictor/src/common/config.py
+++ ai/test-predictor/src/common/config.py
@@ -35,7 +35,7 @@ ADAM_LEARNING_RATE = 0.0001
 
 # Sequence Length (1): How many historical runs to look at. 
 # 1 = Current run only. 10 = Look at the last 10 results.
-SEQUENCE_LENGTH = 25
+SEQUENCE_LENGTH = 10
 # The "width" of the data. It tells the AI exactly how many different pieces of information it gets for every single run.
 # Currently Duration (ms), Attempt number, Verb (encoded), Level (encoded), Backend (encoded), System (encoded), Name (encoded) and Scenario (encoded).
 NUM_FEATURES = 9
